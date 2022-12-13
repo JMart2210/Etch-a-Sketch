90872f22affe6cc830dc727e333f81733071ea5b
Jeremy Martin <46061444+JMart2210@users.noreply.github.com>
2022-12-13 15:01:03 -0500

    squares fill when clicked or when passed over with button held down

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const board = document.querySelector('.board');
-
 const input = document.querySelector('.size');
 
 input.addEventListener('change', () => {
     const userInput = input.value;
-    while (board.firstChild) board.removeChild(board.firstChild); //removes all the squares from the grid
+    //removes all the squares from the previous grid
+    while (board.firstChild) board.removeChild(board.firstChild); 
     createGrid(userInput);
 })
 
 function createGrid(userInput) {
 for (let rows = 0; rows < userInput; rows++) 
-{
+    {
     const row = document.createElement('div');
     board.append(row);
 
     for (let column = 0; column < userInput; column++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);        
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('square');
+        row.appendChild(newDiv); 
+        newDiv.addEventListener("mousedown", darkenSquare);    
+        newDiv.addEventListener("mouseover", mouseMove);
+        }     
     }
 }
+
+//checks if 
+function mouseMove(e) {
+    if (e.buttons == 1) darkenSquare(e);
+}
+//sets background to black
+function darkenSquare(e){
+    e.target.setAttribute('style', 'background: black;'); 
 }
